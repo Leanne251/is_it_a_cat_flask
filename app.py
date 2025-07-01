07b9f4457b6fb8e230b72cf84a4a3afa7c039ab5
Leanne <leannesmith251@gmail.com>
2025-07-01 21:40:16 +0100

    changed port for render

--- app.py
+++ app.py
@@ -83,7 +83,9 @@ def predict():
    
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # 5000 as fallback
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
